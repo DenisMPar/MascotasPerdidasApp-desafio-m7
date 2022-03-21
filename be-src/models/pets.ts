09938aec41bf80315c16c4a3a336bfcc60b101aa
DenisMPar <paradadenismartin@gmail.com>
2022-03-21 14:12:17 -0300

    added length

--- be-src/models/pets.ts
+++ be-src/models/pets.ts
@@ -5,7 +5,7 @@ export class Pet extends Model {}
 Pet.init(
   {
     name: DataTypes.STRING,
-    pictureUrl: DataTypes.STRING,
+    pictureUrl: DataTypes.STRING(1000),
     lat: DataTypes.FLOAT,
     lng: DataTypes.FLOAT,
     condition: DataTypes.STRING,
